80cd678582e596d49ccef5ffa4e971e0221bd3c3
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-10-29 15:23:27 -0700

    Initial Commit

---
+++ src/main/java/com/tcg/recordtime/Main.java
@@ -0,0 +1,12 @@
+package com.tcg.recordtime;
+
+/**
+ * Created by JoseR on 10/29/2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
