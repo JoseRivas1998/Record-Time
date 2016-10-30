2bff3ec36e4d314ce427d963bcc866094f673dd2
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-10-29 19:22:38 -0700

    Create frame layout

--- src/main/java/com/tcg/recordtime/components/frames/CreateProfileFrame.java
+++ src/main/java/com/tcg/recordtime/components/frames/CreateProfileFrame.java
@@ -4,6 +4,7 @@ import com.tcg.recordtime.managers.FileManager;
 import org.json.JSONObject;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 /**
@@ -11,11 +12,45 @@ import java.awt.*;
  */
 public class CreateProfileFrame extends JFrame {
 
-    public CreateProfileFrame() throws HeadlessException {
+    public CreateProfileFrame() {
+        this(null);
+    }
+
+    public CreateProfileFrame(Component relative) {
         JSONObject profiles = new JSONObject(FileManager.loadProfilesJSON(true).toString());
-        
-        setSize(100, 100);
-        setLocationRelativeTo(null);
+        setTitle("Create Profile - Record Time");
+
+
+        JLabel title = new JLabel("Create Profile");
+        title.setHorizontalAlignment(JLabel.CENTER);
+
+        JTextField profileIn = new JTextField(10);
+
+        JButton create = new JButton("Create");
+        create.addActionListener(e -> {
+            JOptionPane.showMessageDialog(null, "test", "test", JOptionPane.INFORMATION_MESSAGE);
+        });
+
+        JPanel inputPanel = new JPanel();
+        inputPanel.add(profileIn);
+        inputPanel.add(create);
+
+        JPanel titlePanel = new JPanel();
+
+        JPanel contentPanel = new JPanel();
+        contentPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        contentPanel.setLayout(new BorderLayout());
+
+        contentPanel.add(title, BorderLayout.NORTH);
+        contentPanel.add(inputPanel, BorderLayout.CENTER);
+
+        getContentPane().setLayout(new BorderLayout());
+        getContentPane().add(contentPanel);
+
+        pack();
+        setLocationRelativeTo(relative);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setResizable(false);
         setVisible(true);
     }
 }
